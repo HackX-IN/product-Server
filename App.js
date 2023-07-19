153996e985228e3f8725aac09c7397f251b41d77
INAMUL HASAN <114344918+HackX-IN@users.noreply.github.com>
2023-07-19 11:05:13 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ const mongoose=require('mongoose')
 const productRouter=require('./Router/Product')
 const userRouter=require('./Router/User')
 const cors = require('cors');
+const cookieParser=require('cookie-parser')
 
 
 dotenv.config()
@@ -16,6 +17,8 @@ app.use(cors({ origin: "http://localhost:19000", optionsSuccessStatus: 200 }));
 
 app.use(express.json({limit:'10mb'}))
 app.use(express.urlencoded({extended:true,limit:'10mb'}))
+app.use(cookieParser())
+
 
 mongoose.connect(process.env.MONGO_URL).then(()=>console.log('DB Connected'))
 
